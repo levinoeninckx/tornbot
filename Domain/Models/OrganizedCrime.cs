e69d493e51df2b9cab6f93e2bfd3b776b8122e12
Levi Noeninckx <levi.noeninckx@protonmail.com>
2026-07-26 15:25:43 +0100

    Add `IsAvailable` and `IsCompleted` computed properties to `OrganizedCrime` model

--- Domain/Models/OrganizedCrime.cs
+++ Domain/Models/OrganizedCrime.cs
@@ -7,4 +7,6 @@ public class OrganizedCrime
     public int Id { get; set; }
     public int CrimeId { get; set; }
     public OrganizedCrimeStatus Status { get; set; } = OrganizedCrimeStatus.Recruiting;
+    public bool IsAvailable => Status is OrganizedCrimeStatus.Recruiting or OrganizedCrimeStatus.Planning;
+    public bool IsCompleted => Status is OrganizedCrimeStatus.Successful or OrganizedCrimeStatus.Failure;
 }
